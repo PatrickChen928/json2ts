19e725d1db5b259536b5d36bdcf9ea5ca58f4265
chen <786156072@qq.com>
2022-03-17 14:35:23 +0800

    chore: format

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,3 @@
 export * from './parse';
 export * from './transform';
+export * from './codegen';
--- src/transform.ts
+++ src/transform.ts
@@ -35,8 +35,6 @@ export function traverser(ast: AstChildNode, visiter: Visiter) {
   return ast;
 }
 
-
-
 export function transform(ast: AstChildNode) {
   traverser(ast, {
     [STRING_TYPE]: {
